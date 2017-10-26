5e432e14aa66c11dcede67db59f05472f294220d
Iván García <alu0100693737@ull.edu.es>
2017-10-26 17:01:00 +0100

    matriz de distancias y tiempo terminada

--- src/rutasTuristicas/Principal.java
+++ src/rutasTuristicas/Principal.java
@@ -1,7 +1,7 @@
 package rutasTuristicas;
 
 public class Principal {
-public static void main(String[] args) {
-	System.out.println("HOLA");
-}
+	public static void main(String[] args) {
+		System.out.println("HOLA");
+	}
 }
--- src/rutasTuristicas/puntosTuristicos.java
+++ src/rutasTuristicas/puntosTuristicos.java
@@ -1,5 +1,10 @@
 package rutasTuristicas;
 
 public class puntosTuristicos {
-
+	private int numeroDiasEstancia;
+	private int numHorasDiarias;
+	
+	public puntosTuristicos() {
+		
+	}
 }
